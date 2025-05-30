1ab404de77816ebf8bfaa3bf1d2e0fb5a667438f
nasswcksn <anaswicaksonoanas@gmail.com>
2025-05-30 14:37:29 +0700

    Add 'About Us' page route to navigation

--- frontend_test/src/app/faq/page.js
+++ frontend_test/src/app/faq/page.js
@@ -58,10 +58,10 @@ export default function FAQPage() {
 						Result
 					</Link>
           <Link 
-            href="/about" 
+            href="/trending" 
             className="text-white font-semibold relative transition-colors duration-200 hover:text-indigo-400 after:content-[''] after:block after:w-0 after:h-0.5 after:bg-indigo-400 after:transition-all after:duration-300 hover:after:w-full after:absolute after:left-0 after:-bottom-1"
           >
-            About Us
+            Trending
           </Link>
 					<Link
 						href="/faq"
