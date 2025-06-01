ea23c034ca4d856bf3200c2a2505efebc83ba087
nasswcksn <anaswicaksonoanas@gmail.com>
2025-06-01 19:02:03 +0700

    Change conection db url from host docker to localhost

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 import sqlalchemy as sa
 from sqlalchemy.orm import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin123@host.docker.internal:5432/faztream_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin123@localhost:5432/faztream_db"
 engine = sa.create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sa.orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
